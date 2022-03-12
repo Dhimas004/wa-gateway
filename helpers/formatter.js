11cc16382bd0d0d5e75c6d78137895a3194679f7
Dhimas004 <dimasyudhatama98@gmail.com>
2022-03-12 14:57:33 +0700

    initial commit

---
+++ helpers/formatter.js
@@ -0,0 +1,19 @@
+const phoneNumberFormatter = function (number) {
+  // menghilangkan karakter selain angka
+  let formatted = number.replace(/\D/g, "");
+
+  // merubah angka 0 menjadi 62
+  if (formatted.startsWith("0")) {
+    formatted = "62" + formatted.substr(1);
+  }
+
+  if (!formatted.endsWith("@c.us")) {
+    formatted += "@c.us";
+  }
+
+  return formatted;
+};
+
+module.exports = {
+  phoneNumberFormatter,
+};
